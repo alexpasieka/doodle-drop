b6525c04ba1eb3c288888a188d9d439a718fe7e8
Alex <alexspasieka@gmail.com>
2019-04-19 21:55:14 -0400

    create reactive masthead

--- client/src/masthead.js
+++ client/src/masthead.js
@@ -1,19 +1,39 @@
-const Masthead = () => {
-	return (
-		<React.Fragment>
-			<h1 id="title">Doodle Drop</h1>
+const Masthead = (props) => {
+	if (props.loggedIn === "true") {
+		return (
+			<React.Fragment>
+				<h1 id="title"><a href="/">Doodle Drop</a></h1>
 
-			<nav>
-				<a href="/user">Account</a>
-				<a href="/logout">Log Out</a>
-			</nav>
-		</React.Fragment>
-	);
+				<nav>
+					<button data-toggle="dropdown" type="button" id="user-menu">{props.username} <i className="fas fa-caret-down"></i></button>
+					<div className="dropdown-menu">
+						<a className="dropdown-item" href="/user">My Doodles</a>
+						<a className="dropdown-item" href="/change-password">Change Password</a>
+						<a className="dropdown-item" href="/logout">Logout</a>
+					</div>
+				</nav>
+			</React.Fragment>
+		);
+	}
+	else {
+		return (
+			<React.Fragment>
+				<h1 id="title">Doodle Drop</h1>
+
+				<nav>
+					<a href="/login">Log In/Sign Up</a>
+				</nav>
+			</React.Fragment>
+		);
+	}
 };
 
 const setup = () => {
+	const loggedIn = document.querySelector("#masthead").dataset.loggedin;
+	const username = document.querySelector("#masthead").dataset.username;
+
 	ReactDOM.render(
-		<Masthead/>,
+		<Masthead loggedIn={loggedIn} username={username}/>,
 		document.querySelector("#masthead")
 	);
 };
--- hosted/src/masthead.js
+++ hosted/src/masthead.js
@@ -1,33 +1,77 @@
 "use strict";
 
-var Masthead = function Masthead() {
-	return React.createElement(
-		React.Fragment,
-		null,
-		React.createElement(
-			"h1",
-			{ id: "title" },
-			"Doodle Drop"
-		),
-		React.createElement(
-			"nav",
+var Masthead = function Masthead(props) {
+	if (props.loggedIn === "true") {
+		return React.createElement(
+			React.Fragment,
 			null,
 			React.createElement(
-				"a",
-				{ href: "/user" },
-				"Account"
+				"h1",
+				{ id: "title" },
+				React.createElement(
+					"a",
+					{ href: "/" },
+					"Doodle Drop"
+				)
 			),
 			React.createElement(
-				"a",
-				{ href: "/logout" },
-				"Log Out"
+				"nav",
+				null,
+				React.createElement(
+					"button",
+					{ "data-toggle": "dropdown", type: "button", id: "user-menu" },
+					props.username,
+					" ",
+					React.createElement("i", { className: "fas fa-caret-down" })
+				),
+				React.createElement(
+					"div",
+					{ className: "dropdown-menu" },
+					React.createElement(
+						"a",
+						{ className: "dropdown-item", href: "/user" },
+						"My Doodles"
+					),
+					React.createElement(
+						"a",
+						{ className: "dropdown-item", href: "/change-password" },
+						"Change Password"
+					),
+					React.createElement(
+						"a",
+						{ className: "dropdown-item", href: "/logout" },
+						"Logout"
+					)
+				)
 			)
-		)
-	);
+		);
+	} else {
+		return React.createElement(
+			React.Fragment,
+			null,
+			React.createElement(
+				"h1",
+				{ id: "title" },
+				"Doodle Drop"
+			),
+			React.createElement(
+				"nav",
+				null,
+				React.createElement(
+					"a",
+					{ href: "/login" },
+					"Log In/Sign Up"
+				)
+			)
+		);
+	}
 };
 
 var setup = function setup() {
-	ReactDOM.render(React.createElement(Masthead, null), document.querySelector("#masthead"));
+	var loggedIn = document.querySelector("#masthead").dataset.loggedin;
+	var username = document.querySelector("#masthead").dataset.username;
+
+	ReactDOM.render(React.createElement(Masthead, { loggedIn: loggedIn, username: username }), document.querySelector("#masthead"));
 };
 
 $(document).ready(setup());
